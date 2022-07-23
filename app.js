af1d6c2ce9e4e14cf3260eb2ec495af4d1aa379a
eunkeeee <eunkeeeee@gmail.com>
2022-07-23 16:53:04 +0900

    2.3 Change Color

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const lineWidth = document.querySelector("#line-width");
+const color = document.querySelector("#color");
 
 canvas.width = 800;
 canvas.height = 800;
@@ -20,6 +21,10 @@ const colors = [
   "#7d5fff",
 ];
 
+function onColorChange(event) {
+  ctx.fillStyle = event.target.value;
+  ctx.strokeStyle = event.target.value;
+}
 function onMouseMove(event) {
   if (isPainting) {
     // 마우스가 지나간 x,y 좌표: event.offsetX/Y로 가져올 수 있음
@@ -37,10 +42,11 @@ function quitPainting() {
   ctx.beginPath();
 }
 function onLineWidthChange(event) {
-  ctx.lineWidth = event.traget.value;
+  ctx.lineWidth = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMouseMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", quitPainting);
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
